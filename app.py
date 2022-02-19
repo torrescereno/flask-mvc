65ddbb0f56700a35628548340e3c1a28a657df63
Luis Torres Cereño <torrescereno@gmail.com>
2022-02-19 14:43:33 -0300

    pre commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from app import create_app
-from config.config import DevelopmentConfig
 
-app = create_app(config=DevelopmentConfig)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 
-from app.database.db import db
-from app.extensions.ext import ma, jwt, migrate
-
-from cli import create_db, create_user, run_test, run_converage
-
-from config.config import DevelopmentConfig
-
 from app.api.views.auth_view import auth_api_bp
-from app.api.views.user_view import user_api_bp
 from app.api.views.home_view import home_api_bp
+from app.api.views.user_view import user_api_bp
+from app.database.db import db
+from app.extensions.ext import jwt, ma, migrate
+from cli import create_db, create_user, run_coverage, run_test
+from config.config import DevelopmentConfig
 
 
 def create_app(config=DevelopmentConfig) -> Flask:
@@ -25,7 +22,7 @@ def create_app(config=DevelopmentConfig) -> Flask:
     app.cli.add_command(create_db)
     app.cli.add_command(create_user)
     app.cli.add_command(run_test)
-    app.cli.add_command(run_converage)
+    app.cli.add_command(run_coverage)
 
     app.register_blueprint(user_api_bp)
     app.register_blueprint(auth_api_bp)
